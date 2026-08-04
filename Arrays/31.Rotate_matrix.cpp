550ff6e91990623a40d72d9947edf21965c99657
Dhanesh-Mhase <dhaneshmhase7@gmail.com>
2026-08-05 00:09:10 +0530

    DSA 31

--- Arrays/31.Rotate_matrix.cpp
+++ Arrays/31.Rotate_matrix.cpp
@@ -9,24 +9,46 @@ int main(){
     int rows=4;
     int cols=4;
 
-    int ans[rows][cols];
 
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            ans[j][rows-1-i]= mat[i][j];
+    // //Brute
+    // int ans[rows][cols];
+
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<cols;j++){
+    //         ans[j][rows-1-i]= mat[i][j];
+    //     }
+    // }
+
+
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<cols;j++){
+    //         cout<<ans[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+  
+    //Optimal
+    //(1)Transpose
+    //(2)Reverse each row
+
+    //Transpose
+    for(int i=0;i<=rows-2;i++){
+        for(int j=i+1;j<=rows-1;j++){
+            swap(mat[i][j],mat[j][i]);
         }
     }
 
+    //Reverse row
+    for(int i=0;i<rows;i++){
+        reverse(mat[i],mat[i]+rows);
+    }
 
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            cout<<ans[i][j]<<" ";
+            cout<<mat[i][j]<<" ";
         }
         cout<<endl;
     }
-
-  
-
-
     
 }
